e5ef8fa2dfc278c0a9f04836e64c2a0f406913da
Lucas Duarte <l.duarte.mk@gmail.com>
2020-04-16 19:20:13 -0300

    métodos HTTP

--- src/index.js
+++ src/index.js
@@ -2,8 +2,29 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello " });
+/**
+ * Método HTTP:
+ *
+ * GET: Buscar informações do back-end
+ * POST: Criar uma iformação no back-end
+ * PUT/PATCH: Alterar uma informação no back-end
+ * DELETE: Deletar informação no back-end
+ */
+
+app.get("/projetos", (request, response) => {
+  return response.json(["projeto1", "projeto2"]);
+});
+
+app.post("/projects", (request, response) => {
+  return response.json(["projeto1", "projeto2", "projeto3"]);
+});
+
+app.put("/projects/:id", (request, response) => {
+  return response.json(["projeto4", "projeto2", "projeto3"]);
+});
+
+app.delete("/projects/:id", (request, response) => {
+  return response.json(["projeto2", "projeto3"]);
 });
 
 app.listen(3333, () => {
