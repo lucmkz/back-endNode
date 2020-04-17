20b8bd14cd23d501f6d60a4f09ca168a35107db6
Lucas Duarte <l.duarte.mk@gmail.com>
2020-04-17 15:01:05 -0300

    params types

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 /**
  * Método HTTP:
  *
@@ -11,15 +13,32 @@ const app = express();
  * DELETE: Deletar informação no back-end
  */
 
-app.get("/projetos", (request, response) => {
+/**
+ * Tipos de parâmetros
+ *
+ * Query Params: Filtro e paginação > Na url vem ?nome=value
+ * Route Params: Na url, depois da barra /2
+ * Request Body: JSON
+ */
+
+app.get("/projects", (request, response) => {
+  const query = request.query; // Para verificar os query params
+  console.log(query);
+
   return response.json(["projeto1", "projeto2"]);
 });
 
 app.post("/projects", (request, response) => {
+  const body = request.body;
+  console.log(body);
+
   return response.json(["projeto1", "projeto2", "projeto3"]);
 });
 
 app.put("/projects/:id", (request, response) => {
+  const params = request.params; // para pegar o id do route params
+
+  console.log(params);
   return response.json(["projeto4", "projeto2", "projeto3"]);
 });
 
