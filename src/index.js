8496634c93724f13648de60a7fc078d9abf45bfc
Lucas Duarte <l.duarte.mk@gmail.com>
2020-04-17 16:46:51 -0300

    functional app

--- src/index.js
+++ src/index.js
@@ -43,13 +43,34 @@ app.post("/projects", (request, response) => {
 
 app.put("/projects/:id", (request, response) => {
   const { id } = request.params;
+  const { title, owner } = request.body;
+  const projectIndex = projects.findIndex((project) => project.id === id);
 
-  console.log(params);
-  return response.json(["projeto4", "projeto2", "projeto3"]);
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: "project not find" });
+  }
+
+  const project = {
+    id,
+    title,
+    owner,
+  };
+
+  projects[projectIndex] = project;
+
+  return response.json(project);
 });
 
 app.delete("/projects/:id", (request, response) => {
-  return response.json(["projeto2", "projeto3"]);
+  const { id } = request.params;
+  const projectIndex = projects.findIndex((project) => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: "project not find" });
+  }
+
+  projects.splice(projectIndex, 1);
+  return response.status(204).send();
 });
 
 app.listen(3333, () => {
